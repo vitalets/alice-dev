d70deb107d8175159d3c466e18f3f92d84a9f9fe
vitalets <vitalets@yandex-team.ru>
2019-11-06 20:59:45 +0300

    release-script

--- scripts/release-backend.sh
+++ scripts/release-backend.sh
@@ -4,7 +4,7 @@ echo "SYNC SOURCES..."
 rsync -avz --delete -e 'ssh' --exclude 'frontend' ./src ./package{,-lock}.json ya-cloud:/srv/alice-dev-backend
 
 echo "INSTALLING DEPS..."
-ssh ya-cloud "cd /srv/alice-dev-backend && npm ci --only=prod"
+ssh ya-cloud "cd /srv/alice-dev-backend && npm install --only=prod"
 
 echo "RESTARTING PM2..."
 ssh ya-cloud "pm2 reload /srv/ecosystem.config.js --only alice-dev-backend --update-env"
