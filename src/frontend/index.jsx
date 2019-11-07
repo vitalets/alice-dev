a841e53e1bf4edc641bcc116a5ae45864009a353
vitalets <vitalets@yandex-team.ru>
2019-11-07 11:33:08 +0300

    add sentry

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,8 +1,11 @@
+import * as Sentry from '@sentry/browser';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './components/App';
 import AppController from './controllers/App';
+
+Sentry.init({dsn: 'https://ef09734ff94541ec9c3ba5b577fdee57@sentry.io/1810772'});
 
 ReactDOM.render(
   <Provider store={store}>
