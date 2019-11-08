ab035cfda98d9aa8726bbc7838755019d16b4d32
vitalets <vitalets@yandex-team.ru>
2019-11-08 08:54:30 +0300

    add sentry on backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,6 +2,9 @@
  * Entry point.
  */
 
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://a2f9dd5d423c433ab15c8ca9e9aad1f0@sentry.io/1812312' });
+
 const Server = require('./server');
 const logger = require('loggee').create('index');
 
