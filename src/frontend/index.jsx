fd6e164e92f6954fa15a3a96de0f45e04c5dbbef
vitalets <vitalets@yandex-team.ru>
2019-11-21 20:37:57 +0300

    sentry: show errors in console

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -5,7 +5,17 @@ import { store } from './store'
 import App from './components/App';
 import AppController from './controllers/App';
 
-Sentry.init({dsn: 'https://ef09734ff94541ec9c3ba5b577fdee57@sentry.io/1810772'});
+Sentry.init({
+  dsn: 'https://ef09734ff94541ec9c3ba5b577fdee57@sentry.io/1810772',
+  // to show errors in console
+  // see: https://github.com/getsentry/sentry-javascript/issues/1600
+  beforeSend: (event, hint) => {
+    setTimeout(() => console.error(hint.originalException || hint.syntheticException), 0);
+    return event;
+  }
+});
+
+setTimeout(() => {throw new Error('abc')}, 5000);
 
 ReactDOM.render(
   <Provider store={store}>
